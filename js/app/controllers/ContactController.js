723540e7f78eb0c24ffaafb05c82da823b9a88d3
hudbuz <hzb2@cornell.edu>
2016-09-06 18:32:05 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,17 +1,27 @@
 function ContactController() {
     this.contacts = [{
         name: 'Bob',
-        phone: '0123458690'
+        phoneNumber: '0123458690'
     },{
         name: 'Tim',
-        phone: '3934203242'
+        phoneNumber: '3934203242'
     },{
         name: 'Ross',
-        phone: '0684059433'
+        phoneNumber: '0684059433'
     }];
 
     this.removeContact = function (index) {
+        
         this.contacts.splice(index, 1);
+    }
+
+
+    this.addContact = function () {
+
+        this.contacts.push({
+            name: this.name,
+            phoneNumber: this.phoneNumber
+        });
     }
 }
 
